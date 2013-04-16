f15d544244cd11d264a0d40f01b143fa942dd3b0
Matthias Putz <matthias.putz@esrlabs.com>
2013-04-16 12:02:17 +0200

    fixed self update problem..

--- subcmds/sync.py
+++ subcmds/sync.py
@@ -698,8 +698,8 @@ def _VerifyTag(project):
         return False
 
     env = os.environ.copy()
-    env['GIT_DIR'] = project.gitdir.encode()
-    env['GNUPGHOME'] = gpg_dir.encode()
+    env['GIT_DIR'] = project.gitdir
+    env['GNUPGHOME'] = gpg_dir
 
     cmd = [GIT, 'tag', '-v', cur]
     proc = subprocess.Popen(cmd,
