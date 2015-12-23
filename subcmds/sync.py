e58d992283893763e113324363c7fec64cc981e6
Matthias Putz <matthias.putz@esrlabs.com>
2015-12-23 11:26:59 +0100

    Modify: change config dir to be esrlabs specific

--- subcmds/sync.py
+++ subcmds/sync.py
@@ -797,7 +797,7 @@ def _PostRepoFetch(rp, no_repo_verify=False, verbose=False):
             file=sys.stderr)
 
 def _VerifyTag(project):
-  gpg_dir = os.path.expanduser('~/.repoconfig/gnupg')
+  gpg_dir = os.path.expanduser('~/.repoconfig-esrlabs/gnupg')
   if not os.path.exists(gpg_dir):
     print('warning: GnuPG was not available during last "repo init"\n'
           'warning: Cannot automatically authenticate repo."""',
